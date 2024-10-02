7289d38f48149c22b77733de51329e7442760f43
AndreyAndrey1980 <mandibul1@mail.ru>
2024-10-02 20:32:08 +0300

    add product and category models

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    price = models.IntegerField(null=True)
+    create_date = models.DateTimeField(default=datetime.now)
+    last_change_date = models.DateTimeField(default=datetime.now)
+    image = models.ImageField(upload_to='images', null=True)
